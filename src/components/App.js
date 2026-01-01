5a65c7df352f7ae0be128c54036933b6353cf245
Yashwanth M <yashwanthm8055@gmail.com>
2026-01-01 12:24:20 +0530

    Render multiple dream projects with required data attributes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import "./styles.css";
 
-const App = () => {
+function App() {
+  const projects = [
+    {
+      name: "Portfolio Website",
+      description: "Personal portfolio showcasing React projects and skills."
+    },
+    {
+      name: "Blog Platform",
+      description: "Full-featured blog with Markdown editor and comments."
+    },
+    {
+      name: "Fitness Tracker",
+      description: "Track workouts, calories, and progress with charts."
+    }
+  ];
+
   return (
-    <div id="main"></div>
-  )
+    <div className="ns-wrapper">
+      {projects.map((project, index) => (
+        <div key={index}>
+          <h1 data-ns-test="project-name">{project.name}</h1>
+          <h6 data-ns-test="project-description">{project.description}</h6>
+        </div>
+      ))}
+    </div>
+  );
 }
-
 
 export default App;
